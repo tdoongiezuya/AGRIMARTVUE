60829f3934c9e974207d786c738d6565a07d7d36
Ludwig Manansala <l.manansala+wlth@arcanys.com>
2024-04-18 19:43:59 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
