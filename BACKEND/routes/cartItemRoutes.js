6cc308838ccdf5f48cb766b2792a60c7f411310f
lemnyxx <lemnyx@protonmail.com>
2024-05-06 20:21:06 +0800

    specified naming for routes

--- BACKEND/routes/cartItemRoutes.js
+++ BACKEND/routes/cartItemRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const cartItemController = require('../controllers/cartItemController');
 
 // Create a new cart item
-router.post('/cart-items', cartItemController.createCartItem);
+router.post('/createCartItem', cartItemController.createCartItem);
 
 // Get all cart items
-router.get('/cart-items', cartItemController.getAllCartItems); // Add this line
+router.get('/getAllCartItems', cartItemController.getAllCartItems); // Add this line
 
 // Get cart item by ID
-router.get('/cart-items/:cart_item_id', cartItemController.getCartItemById);
+router.get('/getCartItemById/:cart_item_id', cartItemController.getCartItemById);
 
 // Update a cart item
-router.put('/cart-items/:cart_item_id', cartItemController.updateCartItem);
+router.put('/updateCartItem/:cart_item_id', cartItemController.updateCartItem);
 
 // Delete a cart item
-router.delete('/cart-items/:cart_item_id', cartItemController.deleteCartItem);
+router.delete('/deleteCartItem/:cart_item_id', cartItemController.deleteCartItem);
 
 module.exports = router;
--- BACKEND/routes/reviewRoutes.js
+++ BACKEND/routes/reviewRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const reviewController = require('../controllers/reviewController');
 
 // Create a new review
-router.post('/', reviewController.createReview);
+router.post('/createReview', reviewController.createReview);
 
 // Get reviews for a product
-router.get('/:product_id', reviewController.getProductReviews);
+router.get('/getProductReview/:product_id', reviewController.getProductReviews);
 
 // Update a review
-router.put('/:review_id', reviewController.updateReview);
+router.put('/updateReview/:review_id', reviewController.updateReview);
 
 // Delete a review
-router.delete('/:review_id', reviewController.deleteReview);
+router.delete('/deleteReview/:review_id', reviewController.deleteReview);
 
 module.exports = router;
